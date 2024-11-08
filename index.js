c88ef28a92b885ceec16f86b5f8d1bece05d9f80
gairik <gairikgmandal@gmail.com>
2024-11-08 17:19:05 +0530

    bug

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const rightClick1 = document.querySelector('.right1');
 let i = 0, index1 = 0;
 
 const arr = [
-    "images/project2/373006816-8a7d0ab1-bd76-4930-a1a6-ef8c9b795d68.png",
-    "images/project2/373006905-4671dfcb-c512-4d23-bd88-9e8419578091.png",
-    "images/project2/373006958-520720a6-54ac-4de0-ae58-e1be17da1d68.png"
+    "public/images/project2/373006905-4671dfcb-c512-4d23-bd88-9e8419578091.png",
+    "public/images/project2/373006816-8a7d0ab1-bd76-4930-a1a6-ef8c9b795d68.png",
+    "public/images/project2/373006958-520720a6-54ac-4de0-ae58-e1be17da1d68.png"
 ];
 const images1 = [
-    "images/project1/376517874-a7447dda-a49f-4d8a-8b43-c58497bf21ff.png",
-    "images/project1/376518279-aef42465-08cf-4a00-9e80-db259f7bb5a1.png",
-    "images/project1/376518763-19e2d1bf-48c4-45e3-9aa9-8c1bc3e0a1ea.png",
-    "images/project1/376519218-16f6d0db-b4f4-41e7-8913-f1f43c83f07f.png",
-    "images/project1/376519303-5471648e-feaf-4df1-8096-1215f5735bd5.png"
+    "public/images/project1/376517874-a7447dda-a49f-4d8a-8b43-c58497bf21ff.png",
+    "public/images/project1/376518279-aef42465-08cf-4a00-9e80-db259f7bb5a1.png",
+    "public/images/project1/376518763-19e2d1bf-48c4-45e3-9aa9-8c1bc3e0a1ea.png",
+    "public/images/project1/376519218-16f6d0db-b4f4-41e7-8913-f1f43c83f07f.png",
+    "public/images/project1/376519303-5471648e-feaf-4df1-8096-1215f5735bd5.png"
 ]
 
 function updateImage(direction) {
